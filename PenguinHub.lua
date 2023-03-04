9d11ff28d3887710e7932df31a57e047f88e0a28
ScriptyDev <77843599+aaa826@users.noreply.github.com>
2023-03-04 12:26:35 -0500

    Create PenguinHub.lua

---
+++ PenguinHub.lua
@@ -0,0 +1,7 @@
+if game.PlaceId == 155615604 then
+  
+loadstring(game:HttpGet("https://raw.githubusercontent.com/aaa826/123/main/PenguinGames/Protected%20(4).lua"))()
+elseif game.PlaceId == 1215581239 then
+  
+loadstring(game:HttpGet("https://raw.githubusercontent.com/aaa826/123/main/PenguinGames/Protected%20(3).lua"))()
+end
