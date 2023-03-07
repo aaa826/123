1b041cb1d1f90215dbc36102cf2a16fe5d2cc798
ScriptyDev <77843599+aaa826@users.noreply.github.com>
2023-03-07 16:02:17 -0500

    Update PenguinHub.lua

--- PenguinHub.lua
+++ PenguinHub.lua
@@ -1,8 +1,6 @@
 if game.PlaceId == 155615604 then
-  
 loadstring(game:HttpGet("https://raw.githubusercontent.com/aaa826/123/main/PenguinGames/Protected%20(4).lua"))()
 elseif game.PlaceId == 1215581239 then
-  
 loadstring(game:HttpGet("https://raw.githubusercontent.com/aaa826/123/main/PenguinGames/Protected%20(3).lua"))()
 elseif game.PlaceId == 3956818381 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/aaa826/123/main/PenguinGames/NinjaLegends.lua"))()
